6a417be816ac0f5aaca5722aa36852f51046a493
zigakranjc <zigakranjc8@gmail.com>
2025-12-05 10:42:30 +0100

    Add files via upload

--- skripta.js
+++ skripta.js
@@ -3,8 +3,8 @@ var context = canvas.getContext("2d");
 
 
     var img = new Image();
-    img.src = "img/apple.png";  // ali "slika.svg" / "slika.png"
+    img.src = "img/apple.png";  
 
     img.onload = function() {
-        context.drawImage(img, 0, 0, canvas.width, canvas.height);  // nariše sliko v kot (0,0)
+        context.drawImage(img, 75, 70, 100, 100);  // nariše sliko v kot (0,0)
     };
