b6abf491d011db5034718e126aef7f4771aee03b
zigakranjc <zigakranjc8@gmail.com>
2026-01-12 08:10:38 +0100

    Add files via upload
    Bezier curve animation

--- skripta.js
+++ skripta.js
@@ -23,6 +23,7 @@ var ctx= canvas.getContext("2d");
       ctx.fillStyle = "black";
       ctx.strokeStyle = "black";
 
+      function drawPath() {
       ctx.beginPath();
       ctx.moveTo(937, 279);
       ctx.bezierCurveTo(876, 278, 733, 355, 750, 499);
@@ -38,8 +39,49 @@ var ctx= canvas.getContext("2d");
       ctx.bezierCurveTo(903, 492, 1046, 502, 1105, 603);
       ctx.bezierCurveTo(988, 660, 951, 881, 1128, 968);
       ctx.bezierCurveTo(1101, 1046, 1008, 1278, 864, 1205);
+      
       ctx.closePath();
-
-      ctx.fill();
-      ctx.stroke();
-};
+    }
+  
+    // === ANIMACIJA RISANJA ===
+    const dashLength = 3000;
+    let dashOffset = dashLength;
+    let state = "draw"; // draw | hold | reset
+  
+    ctx.setLineDash([dashLength]);
+  
+    function animate() {
+      if (state === "draw") {
+        ctx.clearRect(0, 0, originalWidth, originalHeight);
+        drawPath();
+  
+        ctx.lineDashOffset = dashOffset;
+        ctx.stroke();
+  
+        dashOffset -= 25;
+  
+        if (dashOffset <= 0) {
+          ctx.setLineDash([]);
+          ctx.fill();
+          ctx.stroke();
+          state = "hold";
+  
+          // ⏸️ slika OSTANE vidna
+          setTimeout(() => {
+            state = "reset";
+          }, 3000); // koliko časa ostane narisana
+        }
+      }
+  
+      else if (state === "reset") {
+        ctx.clearRect(0, 0, originalWidth, originalHeight);
+        dashOffset = dashLength;
+        ctx.setLineDash([dashLength]);
+        state = "draw";
+      }
+  
+      requestAnimationFrame(animate);
+    }
+  
+    animate();
+  };
